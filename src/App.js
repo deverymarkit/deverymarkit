6db291d28281e3e585eae864c45988a74efe7038
ssujinchoi <wcsjinn@naver.com>
2022-12-14 22:33:45 +0900

    Feat : 판매중인상품 컴포넌트 css적용 (#20)
    판매중인 상품 컴포넌트 마크업 및 CSS를 적용하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import LoginMain from "./pages/loginPage/LoginMain";
-import Login from "./pages/loginPage/Login";
-import Signup from "./pages/loginPage/Signup";
-import ProfileSetting from "./pages/loginPage/ProfileSetting";
-import Splash from "./pages/splashPage/Splash";
+//import LoginMain from "./pages/loginPage/LoginMain";
+//import Login from "./pages/loginPage/Login";
+//import Signup from "./pages/loginPage/Signup";
+//import ProfileSetting from "./pages/loginPage/ProfileSetting";
+//import Splash from "./pages/splashPage/Splash";
 import Profile from "./pages/profilePage/Profile";
 
 function App() {
 	return (
 		<div className="App">
+			<Profile />
 		</div>
 	);
 }
--- src/components/product/SaleProduct.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Product() {
-    return (
-        <div>
-            상품리스트페이지
-        </div>
-    )
-}
