1a8727a9abae5fe65e9a9f40f91edf8297ae6206
CocoLeahBae <mimijunb@gmail.com>
2022-12-14 17:51:12 +0900

    1214_jh/home

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Login from "./pages/loginPage/Login";
 import Signup from "./pages/loginPage/Signup";
 import ProfileSetting from "./pages/loginPage/ProfileSetting";
 import Splash from "./pages/splashPage/Splash";
+import Home from "./pages/homePage/Home";
 
 function App() {
-	return (
-		<div className="App">
-			<Splash/>
-		</div>
-	);
+    return (
+        <div className="App">
+            <Home />
+        </div>
+    );
 }
 export default App;
--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./home.module.css";
 
 export default function Home() {
     return (
         <div>
+            <header className="topMainNav">
+                <h1>데브리마Kit 피드</h1>
+                <button type="button"></button>
+            </header>
+            <main className="homeContent_nonFeed">
+                <p>유저를 검색해 팔로우 해보세요!</p>
+                <button type="button" className="searchBtn">
+                    검색하기
+                </button>
+            </main>
         </div>
-    )
+    );
 }
