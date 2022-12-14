14a13093af185212c57948545544f8055568f766
2DC <92588154+2duckchun@users.noreply.github.com>
2022-12-14 15:25:48 +0900

    Feat : Splash 컴포넌트 마크업 (#12)
    Feat : Splash 컴포넌트 마크업

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import LoginMain from "./pages/loginPage/LoginMain";
 import Login from "./pages/loginPage/Login";
 import Signup from "./pages/loginPage/Signup";
 import ProfileSetting from "./pages/loginPage/ProfileSetting";
+import Splash from "./pages/splashPage/Splash";
 
 function App() {
 	return (
 		<div className="App">
-			<ProfileSetting/>
+			<Splash/>
 		</div>
 	);
 }
