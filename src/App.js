b3ec5d8dcd162eb9f94ec1165b57857e781cddc2
2DC <92588154+2duckchun@users.noreply.github.com>
2022-12-14 16:58:49 +0900

    Feat : common/navbar 컴포넌트 마크업 (#16)
    1. 화면 대부분에 사용되는 navbar의 마크업과 스타일링을 마무리함.
    2. 추후 내부 로직 수정해야함.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Login from "./pages/loginPage/Login";
 import Signup from "./pages/loginPage/Signup";
 import ProfileSetting from "./pages/loginPage/ProfileSetting";
 import Splash from "./pages/splashPage/Splash";
+import Navbar from "./components/common/navbar/Navbar";
 import Profile from "./pages/profilePage/Profile";
 
 function App() {
 	return (
 		<div className="App">
+			<Navbar/>
 		</div>
 	);
 }
