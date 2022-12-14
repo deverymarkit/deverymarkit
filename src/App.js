f034dd2e13373665666815f6a8d634d5196943b2
2DC <kkts9308@gmail.com>
2022-12-14 09:04:38 +0900

    feat : 폴더 정리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Login from './pages/login/Login';
+import Loginemail from './pages/login/Loginemail';
 
 function App() {
   return (
     <div className="App">
+      <Loginemail></Loginemail>
     </div>
   );
 }
