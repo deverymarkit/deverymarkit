d0b57cb138574baeca9229385f3563c694d2dd37
2DC <kkts9308@gmail.com>
2022-12-15 09:46:40 +0900

    Feat : 제목 (#22)
    1. BasicHeader에 Header의 템플릿을 만들어서 달아놓았음.
    2. Header 작업할 때 사용 요망.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
+import BasicHeader from "./components/common/header/BasicHeader";
+import Navbar from "./components/common/navbar/Navbar";
 
 function App() {
 	return (
 		<div className="App">
+			<BasicHeader/>
+			<Navbar/>
 		</div>
 	);
 }
---
+++ src/components/common/header/BasicHeader.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import style from "./basicHeader.module.css"
+
+export default function BasicHeader() {
+    return (
+        <section className={style.sect_basicheader}>
+            
+        </section>
+    )
+}
