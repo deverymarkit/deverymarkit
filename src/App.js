b31b82a2fb9619ef1c8784e365c2c2500ff082c4
mimijunb <mimijunb@gmail.com>
2022-12-15 11:32:41 +0900

    Feat : Home페이지 마크업

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Signup from "./pages/loginPage/Signup";
 import ProfileSetting from "./pages/loginPage/ProfileSetting";
 import Splash from "./pages/splashPage/Splash";
 import Home from "./pages/homePage/Home";
+import HomehasFollower from "./pages/homePage/HomehasFollower";
 
 function App() {
     return (
         <div className="App">
-            <Home />
+            <HomehasFollower />
         </div>
     );
 }
--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import "./home.module.css";
+import style from "./home.module.css";
+import bulbBlackImg from "../../assets/imgs/bulb_black.png";
 
 export default function Home() {
     return (
-        <div>
-            <header className="topMainNav">
-                <h1>데브리마Kit 피드</h1>
-                <button type="button"></button>
-            </header>
+        <section className={style.cont_home}>
             <main className="homeContent_nonFeed">
-                <p>유저를 검색해 팔로우 해보세요!</p>
-                <button type="button" className="searchBtn">
+                <img src={bulbBlackImg} className={style.img_bulbBlack}></img>
+                <p className={style.txt_user}>유저를 검색해 팔로우 해보세요!</p>
+                <button type="button" className={style.searchBtn}>
                     검색하기
                 </button>
             </main>
-        </div>
+        </section>
     );
 }
