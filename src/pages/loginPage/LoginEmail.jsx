0153eaf594cb5d0cf800787e32d6f86e22604c20
2DC <92588154+2duckchun@users.noreply.github.com>
2022-12-15 17:07:46 +0900

    Feat : loginemail 페이지 유효성 검사 기능 (#29)
    1. 이메일, 비밀번호가 입력되지 않으면 버튼이 비활성화되는 기능 구현

--- src/pages/loginPage/LoginEmail.jsx
+++ src/pages/loginPage/LoginEmail.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from "./loginEmail.module.css"
 
 export default function Login() {
+    const [isInputEmail, setIsInputEmail] = useState("")
+    const [isInputPassword, setIsInputPassword] = useState("")
+    const [isDisabled, setIsDisabled] = useState(true)
+    const [btnColor, setButtonColor] = useState("white")
+    const [btnTxtColor, setBtnTxtColor] = useState("gray")
+
+    // 유효성 검사를 진행합니다.
+    useEffect(() => {
+        if (isInputEmail !== "" && isInputPassword.length > 5) {
+            console.log("펄스아니지롱");
+            setBtnTxtColor("white")
+            setButtonColor("#0F0B19");
+            setIsDisabled(false);
+        } else {
+           console.log("펄스지롱");
+           setBtnTxtColor("gray")
+           setButtonColor("white");
+           setIsDisabled(true);
+        };
+    }, [isInputEmail, isInputPassword])
+
+    // input 엘리먼트에 이벤트가 일어나는 것을 감지합니다.
+    const checkInput = (event) => {
+        if (event.target.name === "email") setIsInputEmail(event.target.value); 
+        else if (event.target.name === "password") setIsInputPassword(event.target.value);
+    }
+
     return (
         <section className={style.cont_login}>
             <h1 className={style.tit_login}>
@@ -18,7 +45,9 @@ export default function Login() {
                 <input 
                 type="email" 
                 id="emailInput"
-                className={style.input_login} 
+                name="email"
+                className={style.input_login}
+                onChange={checkInput} 
                 placeholder="이메일 주소를 적어주세요.">
                 </input>
 
@@ -31,12 +60,16 @@ export default function Login() {
                 <input 
                 type="password" 
                 id="passwordInput"
-                className={style.input_login}  
+                name="password"
+                className={style.input_login}
+                onChange={checkInput} 
                 placeholder="비밀번호를 입력하세요.">
                 </input>
 
                 <button 
-                className={style.btn_login}>
+                className={style.btn_login}
+                disabled={isDisabled}
+                style={{backgroundColor: btnColor, color: btnTxtColor}}>
                 로그인
                 </button>
 
