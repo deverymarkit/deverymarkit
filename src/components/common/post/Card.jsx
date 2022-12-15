b7b58eed827c1d8ba086657dff86862a1068db85
ssujinchoi <58455479+ssujinchoi@users.noreply.github.com>
2022-12-15 21:56:03 +0900

    Feat : Card컴포넌트화 및 css적용  (#26) (#34)
    게시글의 레이아웃이 같고 3군데서 공통적으로 사용되기 때문에 하나의 컴포넌트화 하였습니다.
    피그마를 참고하여 css를 적용하였습니다.

--- src/components/common/post/Card.jsx
+++ src/components/common/post/Card.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import BasicProfileImg from '../BasicProfileImg'
-import style from './Card.module.css'
+import React from "react"
+
+import BasicProfileImg from "../BasicProfileImg"
+import style from "./card.module.css"
 import moreIcon from "../../../assets/imgs/more-vertical.png"
 
-export default function Post() {
+export default function Card() {
     
     return (
-        <section className={style.wrap}>
-        <BasicProfileImg type = "postlist"/>
-        <div className={style.cont_post}>
-            <p className={style.p_nickname}>애월읍 위니브 감귤농장</p>
-            <span >@ weniv_Mandarin</span>
-            <img src={moreIcon} className={style.btn_plus} alt="더보기 버튼"/>
-            <p className={style.p_content}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem repudiandae, dolor vero repellat iste deleniti perferendis fuga? Quos cumque repellendus, repellat et architecto minus dolores consequatur temporibus, commodi fugit accusantium.</p>
-            {/* <img src="" alt="본문 이미지" /> */}
-            <div className={style.box_img}>
-                본문이미지 대체용
+        <article className={style.article_card}>
+            {/*<h3 class="ir">포스트 카드</h3>*/}
+            <div className={style.post_author}>
+                <BasicProfileImg type="post"/>
+                <p className={style.tit_user_name}>애월읍 위니브 감귤농장
+                    <span className={style.tit_user_id}>@ weniv_Mandarin</span>
+                </p>
             </div>
-            <div className={style.cont_btn}>
-                <img src="" alt="하트" />
-                <span className={style.span_count}>58</span>
-                <img src="" alt="댓글" />
-                <span className={style.span_count}>12</span>
+            <div className={style.post_info}>
+                <p className={style.post_contents}>옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할 넣는 풍부하게 뛰노는 인생의 힘있다.
+                </p>
+                {/* 데이터에서 이미지가 있으면 나타내야함. */}
+                <div className={style.box_post}>
+                    이미지 슬라이드
+                </div>
+                <div className={style.box_btn}>
+                    <img src="" alt="좋아요" />
+                    <span className={style.span_count}>58</span>
+                    <img src="" alt="댓글" />
+                    <span className={style.span_count}>12</span>
+                </div>
+                <span className={style.span_post_date}>2022년 12월 13일</span>
+                <img src={moreIcon} className={style.btn_more} alt="더보기" />
             </div>
-            <span>2022년 12월 13일</span>
-        </div>
-    </section>
+        </article>
     )
 }
