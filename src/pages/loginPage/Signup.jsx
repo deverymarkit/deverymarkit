6c10cb18743cc0ca9c2b5d9c8a509ef8f7a33e3d
2DC <kkts9308@gmail.com>
2022-12-16 17:35:09 +0900

    Feat : signup api 적용

--- src/pages/loginPage/Signup.jsx
+++ src/pages/loginPage/Signup.jsx
@@ -1,12 +1,34 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import style from "./signup.module.css"
+import axios from 'axios';
 
 export default function Signup() {
-    const [isInputEmail, setIsInputEmail] = useState("")
-    const [isInputPassword, setIsInputPassword] = useState("")
-    const [isDisabled, setIsDisabled] = useState(true)
-    const [btnColor, setButtonColor] = useState("white")
-    const [btnTxtColor, setBtnTxtColor] = useState("gray")
+    const [isInputEmail, setIsInputEmail] = useState("");
+    const [isInputPassword, setIsInputPassword] = useState("");
+    const [isDisabled, setIsDisabled] = useState(true);
+    const [btnColor, setButtonColor] = useState("white");
+    const [btnTxtColor, setBtnTxtColor] = useState("gray");
+
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
+
+    // axios
+    const 액시오스 = async (emailValue) => {
+        const url = 'https://mandarin.api.weniv.co.kr'
+        try {
+            const 대답 = axios.post(`${url}/user/emailvalid`, {
+                "user": {
+                    "email": emailValue,
+                },
+                "headers": {
+                    "Content-Type": "application/json"
+                }
+            })
+            console.log((await 대답).data.message)
+        } catch(err) {
+            console.log("에러입니다. 휴먼.")
+        }
+    }
 
     // 유효성 검사를 진행합니다.
     useEffect(() => {
@@ -27,6 +49,7 @@ export default function Signup() {
         if (event.target.name === "email") setIsInputEmail(event.target.value); 
         else if (event.target.name === "password") setIsInputPassword(event.target.value);
     }
+    
 
     return (
         <section className={style.cont_signup}>
@@ -45,6 +68,7 @@ export default function Signup() {
                 type="email" 
                 id="emailInput"
                 name="email"
+                ref={emailRef}
                 className={style.input_signup}
                 onChange={checkInput} 
                 placeholder="이메일 주소를 적어주세요.">
@@ -60,6 +84,7 @@ export default function Signup() {
                 type="password" 
                 id="passwordInput"
                 name="password"
+                ref={passwordRef}
                 className={style.input_signup}  
                 onChange={checkInput}
                 placeholder="비밀번호를 입력하세요.">
@@ -68,7 +93,13 @@ export default function Signup() {
                 <button 
                 className={style.btn_signup}
                 disabled={isDisabled}
-                style={{backgroundColor: btnColor, color: btnTxtColor}}>
+                style={{backgroundColor: btnColor, color: btnTxtColor}}
+                onClick={(e) => {
+                    e.preventDefault();
+                    액시오스(emailRef.current.value)
+                    console.log(emailRef.current.value)
+                    console.log(passwordRef.current.value)
+                }}>
                 다음
                 </button>
 
