d2a16704ec2bdc587eb61a5fa7258a9939e61c7e
mimijunb <112460475+CocoLeahBae@users.noreply.github.com>
2022-12-19 10:02:09 +0900

    Feat : Home 컴포넌트 마크업 (#42)
    Co-authored-by: 2DC <kkts9308@gmail.com>

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
-=======
 import style from "./home.module.css";
 import bulbBlackImg from "../../assets/imgs/bulb_black.png";
->>>>>>> 5be8d0a1ac83a7a60947268cdb56706e55bfc591
 
 export default function Home() {
     return (
--- src/pages/homePage/HomehasFollower.jsx
+++ src/pages/homePage/HomehasFollower.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import profileNone from "../../assets/imgs/profile_none.png";
 import style from "./HomehasFollower.module.css";
+import Card from "../../components/common/post/Card";
 
 export default function HomehasFollower() {
     return (
         <section className={style.cont_hasFollower}>
-            <h2>애월읍 위니브 감귤농장</h2>
+            {[1, 2, 3].map(() => (
+                <Card />
+            ))}
         </section>
     );
 }
