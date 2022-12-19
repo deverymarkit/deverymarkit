87afbac9497cd5fd57c30673d4153b3d11662405
mimijunb <mimijunb@gmail.com>
2022-12-19 09:40:01 +0900

    1219first

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import Splash from "./pages/splashPage/Splash";
 import Home from "./pages/homePage/Home";
 import HomehasFollower from "./pages/homePage/HomehasFollower";
 import Router from "./routes/Router";
+import Search from "./pages/searchPage/Search";
 
 function App() {
     return (
         <div className="App">
-            <Router />
+            <Search />
         </div>
     );
 }
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Splash from "../pages/splashPage/Splash"
-import Login from "../pages/loginPage/Login"
-import LoginEmail from "../pages/loginPage/LoginEmail"
-import Signup from "../pages/loginPage/Signup"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Splash from "../pages/splashPage/Splash";
+import Login from "../pages/loginPage/Login";
+import LoginEmail from "../pages/loginPage/LoginEmail";
+import Signup from "../pages/loginPage/Signup";
+import Home from "../pages/homePage/Home";
+import HomehasFollower from "../pages/homePage/HomehasFollower";
 
 export default function Router() {
     return (
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Splash />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/loginemail" element={<LoginEmail />} />
-            <Route path="signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
-    )
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Splash />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/loginemail" element={<LoginEmail />} />
+                <Route path="signup" element={<Signup />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/homehasFollower" element={<HomehasFollower />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
