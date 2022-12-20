1cf811c2d48374c5151c182ea9625f51a8d5862c
2DC <kkts9308@gmail.com>
2022-12-20 15:43:39 +0900

    Feat : Loginemail 로그인 로직 구현, handle 함수 명칭변경 (#54)
    1. Loginemail 컴포넌트의 로그인 로직을 구현함.
    2. 이벤트핸들러에 연결된 함수들 앞에 handle을 명명. (state함수 제외)

--- src/pages/loginPage/LoginEmail.jsx
+++ src/pages/loginPage/LoginEmail.jsx
@@ -1,31 +1,68 @@
 import React, { useEffect, useState } from "react"
+import axios from "axios";
 import style from "./loginEmail.module.css"
 
 export default function Login() {
-    const [isInputEmail, setIsInputEmail] = useState("")
-    const [isInputPassword, setIsInputPassword] = useState("")
-    const [isDisabled, setIsDisabled] = useState(true)
-    const [btnColor, setButtonColor] = useState("white")
-    const [btnTxtColor, setBtnTxtColor] = useState("gray")
+    const [inputEmail, setInputEmail] = useState("");
+    const [inputPassword, setInputPassword] = useState("");
+    const [loginWarning, setLoginWarning] = useState("")
+    const [isDisabled, setIsDisabled] = useState(true);
+    const [btnValid, isBtnValid] = useState(false);
 
     // 유효성 검사를 진행합니다.
     useEffect(() => {
-        if (isInputEmail !== "" && isInputPassword.length > 5) {
-            setBtnTxtColor("white")
-            setButtonColor("#0F0B19");
+        if (inputEmail !== "" && inputPassword.length > 5) {
+            isBtnValid(true);
             setIsDisabled(false);
         } else {
-           setBtnTxtColor("gray")
-           setButtonColor("white");
+           isBtnValid(false)
            setIsDisabled(true);
         };
-    }, [isInputEmail, isInputPassword])
+        setLoginWarning("")
+    }, [inputEmail, inputPassword])
+
 
     // input 엘리먼트에 이벤트가 일어나는 것을 감지합니다.
     const checkInput = (event) => {
-        if (event.target.name === "email") setIsInputEmail(event.target.value); 
-        else if (event.target.name === "password") setIsInputPassword(event.target.value);
+        if (event.target.name === "email") setInputEmail(event.target.value); 
+        else if (event.target.name === "password") setInputPassword(event.target.value);
     }
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        getLogin(inputEmail, inputPassword)
+    }
+
+    const getLogin = async(email, password) => {
+        const url = 'https://mandarin.api.weniv.co.kr/';
+        const userdata = {
+            "email": email,
+            "password": password
+        }
+        try {
+            const loginRes = axios.post(`${url}user/login`, {
+                "user": {
+                    ...userdata
+                },
+                "headers": {
+                    "Content-Type": "application/json"
+                }
+            });
+
+            const loginData = (await loginRes)
+            if (loginData.data.status === 422) {
+                setLoginWarning(loginData.data.message)
+                return
+            };
+
+            if (loginData.data.user) {
+                console.log(loginData.data.user.accountname);
+            };
+
+        } catch(err) {
+            console.log(err);
+        }
+    } 
 
     return (
         <section className={style.cont_login}>
@@ -64,16 +101,19 @@ export default function Login() {
                 placeholder="비밀번호를 입력하세요.">
                 </input>
 
+                <p className={style.p_warning}>{loginWarning}</p>
+
                 <button 
-                className={style.btn_login}
+                className={`${style.btn_login} ${btnValid ? style.valid : style.invalid}`}
                 disabled={isDisabled}
-                style={{backgroundColor: btnColor, color: btnTxtColor}}>
+                onClick={handleLogin}>
                 로그인
                 </button>
 
             </form>
 
-            <button 
+            <button
+            type="button" 
             className={style.btn_join}>
             이메일로 회원가입
             </button>
