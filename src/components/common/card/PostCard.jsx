8ec46425584dfe79eb014a8b4f5063ed3d6166fc
ssujinchoi <wcsjinn@naver.com>
2022-12-24 05:34:27 +0900

    Refact : 게시글 관련 기능 코드 수정 및 axios모듈화 (#71) (#72)
    1. date객체 써서 현재날짜 년/월/일 형태로 나타냄.
    2. 게시글 사진 슬라이드 기능추가
    3. 좋아요 기능추가
    4. 갤러리뷰 사진 넘치는 현상 수정
    5. 통신시에 사용되는 header반복코드를 줄이기 위해 aixos모듈화(#72)

--- src/components/common/card/PostCard.jsx
+++ src/components/common/card/PostCard.jsx
@@ -1,46 +1,127 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Slider from "react-slick";
+import "./slick/slick.css"
+import "./slick/slick-theme.css"
 
+
+import { customAuthAxios } from "../../../api/customAxios";
 import ProfileCard from "./ProfileCard";
+
+
 import style from "./postCard.module.css";
 import moreIcon from "../../../assets/imgs/icon-more-vertical.png";
-import messageIcon from  "../../../assets/imgs/icon-message-small.svg";
+//import messageIcon from  "../../../assets/imgs/icon-message-small.svg";
+import messageIcon from  "../../../assets/imgs/icon-message.svg";
 import heartIcon from  "../../../assets/imgs/icon-heart.svg";
+import heartFillIcon from  "../../../assets/imgs/icon-fill-heart.svg";
+//import heartFillIcon from  "../../../assets/imgs/icon-heart-fill.svg";
+
 
 export default function PostCard({ id, post }) {
 
+    const [isLike, setIsLike] = useState(post.hearted);
+    const [likeCount, setLikeCount] = useState(post.heartCount);
     const navigate = useNavigate();
+
     const handlePostDetail = () => {
         navigate(`/post/${post.id}`)
     }
-
     const handleProfile = () => {
         navigate(`/profile/${post.author.accountname}`)
     }
 
+    const handleLikeToggle = () => {
+        if (isLike === false) {
+            const setLike = async () => {
+                try {
+                    customAuthAxios.post(`/post/${post.id}/heart`);
+                    setLikeCount(likeCount + 1);
+                    setIsLike((prev) => !prev);
+                } catch (err) {
+                    setIsLike(false);
+                    setLikeCount(likeCount);
+                    console.error(err);
+                }
+            }
+            setLike();
+        } else if (isLike === true) {
+            const setUnLike = async () => {
+                try {
+                    customAuthAxios.delete(`/post/${post.id}/unheart`);
+                    setLikeCount(likeCount - 1);
+                    setIsLike((prev) => !prev);
+                } catch (err) {
+                    setIsLike(true);
+                    setLikeCount(likeCount);
+                    console.error(err);
+                }
+            }
+            setUnLike();
+        }
+    }
+
+    //const getPostDetail = async () => {
+    //    try {
+    //        const result = await customAuthAxios.get(`/post/${post.id}`);
+    //        setHeartCount(result.data.heartCount);
+    //    } catch (err) {
+    //        console.error(err);
+    //    }
+    //}
+
+    //useEffect(() => {
+    //    getPostDetail();
+    //}, [isLike])
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        //autoplay: true,s
+    }
+
+    //let postImgDataList = [];
+    //let a = post.image.includes(",")
+    //if (a) {
+    //    postImgDataList = post.image.split(",");
+    //} 
+
     return (
         <article key={id} className={style.article_post_card}>
-            <ProfileCard profileImg={post.author.image} profileState="post" profileName={post.author.username} profileCont={post.author.accountname} handleBtn={handleProfile}/>
-            <div className={style.cont_post_card}>
-                <div className={style.box_post_author} onClick={handlePostDetail}>
-                    <p className={style.post_contents}>{post.content}</p>
+            <div className={style.cont_post_author}>
+                <ProfileCard profileImg={post.author.image} profileState="post" profileName={post.author.username} profileCont={post.author.accountname} handleBtn={handleProfile}/>
+            </div>
+            {
+                post.image ? (
+                    //<div className={style.cont_post_img} >
+                    <Slider {...settings}>
                     {
-                        post.image ? (
-                            <div className={style.box_post}>
-                                <img src={post.image} alt=""/>
-                            </div>
-                        ) : null
+                        post.image.split(",").map((imgsrc, index) => 
+                                <img key={index} src={imgsrc} alt="" onClick={handlePostDetail}/>
+                        )
                     }
-                </div>
+                    </Slider>
+                    //</div>
+                ) : null
+            }
+            <div className={style.cont_post_content}>
+                <p className={style.post_contents}  onClick={handlePostDetail}>{post.content}</p>
                 <div className={style.box_btn}>
-                    <img src={heartIcon} alt="좋아요" />
-                    <span className={style.span_count}>{post.heartCount}</span>
+                    <img src={isLike ? heartFillIcon : heartIcon} alt="좋아요" className={style.btn_like} onClick={handleLikeToggle}/>
+                    <span className={style.span_count}>{likeCount}</span>
                     <img src={messageIcon} alt="댓글" />
                     <span className={style.span_count}>{post.comments.length}</span>
                 </div>
-                <span className={style.span_post_date}>{post.createdAt}</span>
-                <img src={moreIcon} className={style.btn_more} alt="더보기" />
+                <span className={style.span_post_date}>
+                    {
+                        new Intl.DateTimeFormat("ko", { dateStyle: "long" }).format(new Date(post.createdAt))
+                    }
+                </span>
             </div>
+            <img src={moreIcon} className={style.btn_more} alt="더보기" />
         </article>
     )
 }
