073464c53d0cafacdf06fc5344398e88433ed0a5
ssujinchoi <wcsjinn@naver.com>
2022-12-25 22:08:27 +0900

    Refact : 갤러리뷰 아이콘 표시 추가 (#76)
    1. 갤러리뷰 게시글 중 사진이 여러장임을 표시하는 아이콘 추가
    2. slick-slider 화살표 사용하지 않음으로 변경
    3. 갤러리뷰에서 사진이 없어도 flex안에서 요소를 차지하고 있는 게시글이 나타나지 않도록 수정

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,29 +1,17 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
-import BaseURL from "../../components/common/BaseURL";
+import { customAuthAxios } from "../../api/customAxios";
 import style from "./product.module.css";
 
 export default function Product({ accountname, profileType }) {
 
-    // 가짜 데이터
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOTk2MjYwMTdhZTY2NjU4MWM1ZDA4NCIsImV4cCI6MTY3NjY4NzI5MywiaWF0IjoxNjcxNTAzMjkzfQ.nI5e_IgdnZmTHnLcBKvttP8w3AFQ2rnR6NMxAzdTF40"
-    // 가짜 데이터
-
     const [productList, setProductList] = useState([]);
 
     const getProductList = async () => {
-        const url = BaseURL + `/product/${accountname}`;
 
         try {
-            const productRes = axios.get(url, {
-                "headers": {
-                    "Authorization": `Bearer ${token}`,  
-                    "Content-type": "application/json",
-                }
-            })
-            const result = await productRes;
-            setProductList(result.data.product);
+            const productRes = await customAuthAxios.get(`/product/${accountname}`)
+            setProductList(productRes.data.product);
         } catch(err) {
             console.error(err);
         }
