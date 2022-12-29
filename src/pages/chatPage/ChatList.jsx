66c03c841c1fca45f6a28c96ddfc9fe5ffaff19d
이경린 <66896175+lgrin-byte@users.noreply.github.com>
2022-12-29 18:24:29 +0900

    Feature : ChatList 페이지 마크업 (#103)
    * Feat : 모달 배경 CSS 수정
    
    * Fix : Product Modify 버튼 활성화 및 기본 이미지 수정
    수정 시 버튼 활성화 오류 수정
    기본 이미지일 경우 저장 안되도록 설정
    
    * Feat : PostCard 에 모달창 연결(#84)
    
    내가 작성한 포스트일 경우 수정 삭제 (수정기능 미완성 삭제기능 완성)
    다른 사람이 작성한 포스트일 경우 신고하기
    
    * Feat : Chat페이지 마크업(#103)
    
    ChatList페이지 마크업
    네비바 이동하기
    헤더 상단 프로필 더보기버튼 연결

---
+++ src/pages/chatPage/ChatList.jsx
@@ -0,0 +1,59 @@
+import React ,{useState} from 'react'
+import defalutImg from "../../assets/imgs/profile-none.png";
+import Header from '../../components/common/header/Header';
+import Modal from '../../components/common/modal/Modal';
+import ModalPortal from '../../components/common/modal/ModalPortal';
+import Navbar from '../../components/common/navbar/Navbar';
+import style from './chatList.module.css'
+
+export default function ChatList() {
+    const [modalOpen, setModalOpen] = useState(false);
+    // 모달창 노출
+    const showModal = () => {
+        setModalOpen(true);
+    };
+
+    const handleModal = (event) => {
+        // Input을 체크해서 state를 변경하는 함수.
+        if (event.target.name === "수정") console.log(event.target.name); 
+        else if (event.target.name === "삭제") console.log(event.target.name);
+        else console.log(event.target.name);
+    }
+
+
+
+    return (
+        <div className={style.wrap_top_chatList}>
+            <Header type="post" handleHeaderBtn={showModal}/>
+            <div className={style.wrap_chatList}>
+                <div className={style.div_chat}></div>
+                {[1,2,3].map(
+                    (x)=>
+                    
+                    <li className={style.wrap_chat} >
+                        <img className={style.profile_img} src={defalutImg} alt="프로필 이미지" />
+                        <div className={style.icon}></div>
+                        <div className={style.cont_chat}>
+                            <p className={style.name_chat}>애월읍 위니브 감귤농장</p>
+                            <div className={style.cont_span}>
+                                <span className={style.text_chat}>안녕하세요</span>
+                                <span className={style.text_chat}>2022.10.24</span>
+                            </div>
+                        </div>
+                    </li>
+                    
+                )}
+                <ModalPortal>
+                    {modalOpen && 
+                        <Modal  type="profile" 
+                        modalOpen={modalOpen}
+                        setModalOpen={setModalOpen} 
+                        handleModal={handleModal} />    
+                    }    
+                        
+                </ModalPortal>    
+            </div>
+        <Navbar type="chat"/>
+        </div>
+    )
+}
