48dca0dc0951b7e5f7e510e720070a7b893f0c62
ssujinchoi <wcsjinn@naver.com>
2022-12-29 11:07:46 +0900

    Refact : 페이지 로딩중 컴포넌트 추가

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -78,14 +78,13 @@ export default function Product({ accountname }) {
                         <ol className={style.ol_product_list}>
                             {
                                 // 내 프로필이면 모달, 남의 프로필이면 판매링크 넣기
-                                // 가격 단위 콤마 넣기
                                 productList.map((product, id) => 
                                     <li key={id} data-link={product.link} id={product.id} onClick={showModal}>
                                         <figure>
                                             <img src={product.itemImage} className={style.img_product} alt="" />
                                             <figcaption>{product.itemName}</figcaption>
                                         </figure>
-                                        <strong className={style.strong_product_price}>{product.price.toLocaleString()}</strong>
+                                        <strong className={style.strong_product_price}>{product.price.toLocaleString()} 원</strong>
                                     </li>
                                 )
                             }
@@ -107,7 +106,7 @@ export default function Product({ accountname }) {
                             handleModal={handleProductDelete} />} 
             </ModalPortal>    
                 </section>
-                ) : null
+                ) : <></>
             }
         </>
     )
