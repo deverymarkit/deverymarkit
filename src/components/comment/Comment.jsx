2e8957d7e37d2a4c7b14206794d6021a77e73c4e
ssujinchoi <58455479+ssujinchoi@users.noreply.github.com>
2022-12-31 03:44:05 +0900

    Fix : 댓글 등록후 조회시 오류 (#113)
    1. 댓글목록 무한스크롤시 데이터를 중복적으로 불러오거나 랜덤으로 불러오는 오류 수정 (useState함수 비동기실행 문제였음)
    2. 댓글목록에서 프로필 이동하기 추가
    3. 댓글목록 무한스크롤후 마지막 데이터에서 더보기 글씨 나오지 않게 수정

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -6,54 +6,84 @@ import BasicProfileImg from "../common/BasicProfileImg";
 import Loading from "../../pages/Loading";
 import style from "./comment.module.css";
 
-export default function Comment({ post }) {
+export default function Comment({ post, getPost }) {
 
     const loginInfo = JSON.parse(localStorage.getItem("loginStorage"));
     const loginAccountImage = loginInfo.image;
 
-    const [commentList, setCommentList] = useState([]);
-    const [nowComment, setNowComment] = useState("");
     const [isValid, setIsValid] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
+    const [commentList, setCommentList] = useState([]);
+    const [nowComment, setNowComment] = useState("");
     const [skipNumber, setSkipNumber] = useState(0); 
+    const [isMore, setIsMore] = useState(false);
     const target = useRef();
+    const LIMIT = 10;
 
     const getCommentList = async () => {
         try {
-            const commentRes = await customAuthAxios.get(`/post/${post.id}/comments/?limit=10&skip=${skipNumber}`);
-            setCommentList([...commentList, ...commentRes.data.comments]);
-            setIsLoading(false);
+            const commentRes = await customAuthAxios.get(`/post/${post.id}/comments/?limit=${LIMIT}&skip=${skipNumber}`);
+            
+            if (skipNumber === 0) {
+                setCommentList(commentRes.data.comments);
+            } else  {
+                setCommentList([...commentList, ...commentRes.data.comments]);
+                setIsLoading(false);
+                setIsMore(true);
+                if (post.commentCount === commentList.length) {
+                    setIsMore(false);
+                }
+            }
         } catch (err) {
             console.error(err);
         }
     }
 
+    const getNewCommentList = async () => {
+        try {
+            const commentRes = await customAuthAxios.get(`/post/${post.id}/comments/?limit=${LIMIT}&skip=0`);
+            setCommentList(commentRes.data.comments);
+            setIsLoading(false);
+            setSkipNumber(0);
+            if (commentRes.data.comments.length >= 10) {
+                setIsMore(true);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    
+    useEffect(() => {
+        getNewCommentList();
+    }, [])
+
     useEffect(() => {
         getCommentList();
-    }, [skipNumber]);
+    }, [skipNumber])
 
     const loadMore = () => {
         setSkipNumber(prev => prev + 10)
+        
     }
 
     useEffect(() => {
-        if (!isLoading) {
+        if (isMore && target.current) {
             const observer = new IntersectionObserver((entries) => {
                 if (entries[0].isIntersecting) {
                     loadMore();
                 }
             })
-            // 옵저버 탐색 시작
+
             observer.observe(target.current)
         }
     }, [isLoading])
     
-    const handleTyping = (e) => {
+    const handleTypingComment = (e) => {
         setNowComment(e.target.value);
         e.target.value ? setIsValid(true) : setIsValid(false);
     }
 
-    const handleComment = async () => {
+    const handleRegisterComment = async () => {
         if (isValid) {
             try {
                 await customAuthAxios.post(`/post/${post.id}/comments`, {
@@ -62,13 +92,23 @@ export default function Comment({ post }) {
                     }
                 });
                 setNowComment("");
-                setCommentList([]);
-                setSkipNumber(0);
-                getCommentList();
+                setIsLoading(true);
+                getPost();
+                getNewCommentList();
             } catch (err) {
                 console.error(err);
             }
         } 
+    }
+
+    const handleKeyPress = (e) => {
+        //if (e.key === "Enter" && e.shiftKey) {
+        //    setNowComment(prev => prev + `\n`);
+        //} else 
+        if (e.key === "Enter") {
+            handleRegisterComment();
+            e.preventDefault();
+        }
     }
 
     if(isLoading) {
@@ -80,12 +120,12 @@ export default function Comment({ post }) {
                     {
                         commentList.length > 0 && <CommentList commentList={commentList}/>
                     }
-                    <button ref={target}>로딩중</button>
+                    { isMore === true ? <button ref={target}>더보기</button> : null}
                 </div>
                 <div className={style.box_commentInput}>
                     <BasicProfileImg type="comment_list" profileImg={loginAccountImage}/>
-                    <input className={style.inp_comment} type="text" placeholder="댓글 입력하기..." value={nowComment} onChange={handleTyping}/>
-                    <button className={style.btn_commentInput} onClick={handleComment}>게시</button>
+                    <input className={style.inp_comment} type="text" placeholder="댓글 입력하기..." value={nowComment} onChange={handleTypingComment} onKeyDown={handleKeyPress}/>
+                    <button type="button" className={style.btn_commentInput} onClick={handleRegisterComment}>게시</button>
                 </div>
             </>
         )
