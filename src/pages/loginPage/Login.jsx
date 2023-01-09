33c55e32259dbe14107829c294d3c37fce6dd305
2DC <kkts9308@gmail.com>
2023-01-09 21:00:58 +0900

    Refact : Login 소셜 로그인 추가(카카오톡, 구글)

--- src/pages/loginPage/Login.jsx
+++ src/pages/loginPage/Login.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom"
 import React from "react"
 import style from "./login.module.css"
 import deveryLogo from "../../assets/imgs/mark-white.png"
+import googleLogo from "../../assets/imgs/google_logo.png"
+import kakaoLogo from "../../assets/imgs/kakao_logo.png"
 
 export default function Login() {
     const navigate = useNavigate()
@@ -14,10 +16,12 @@ export default function Login() {
             </h1>
         </main>
         <section className={style.cont_login}>
-                <ul className={style.ul_login}>
-                    <li><button className={style.btn_login} onClick={() => {navigate("/loginemail")}}>이메일로 로그인</button></li>
-                    <li><button className={style.btn_join} onClick={() => {navigate("/signup")}}>회원가입</button></li>
-                </ul>
+            <div><button className={style.btn_snslogin}><img src={kakaoLogo} alt="카카오톡 계정으로 로그인" className={style.img_snslogin}/>카카오톡 계정으로 로그인</button></div>
+            <div><button className={style.btn_snslogin}><img src={googleLogo} alt="구글 계정으로 로그인" className={style.img_snslogin}/>구글 계정으로 로그인</button></div>
+            <div className={style.cont_loginsignup}>
+                <div><button className={style.btn_login} onClick={() => {navigate("/loginemail")}}>이메일로 로그인</button></div>
+                <div><button className={style.btn_join} onClick={() => {navigate("/signup")}}>회원가입</button></div>
+            </div>
         </section>
         </>
     )
