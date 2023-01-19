f3078701de25ae4f3b0acc66d4aa463e60145c26
lgrin-byte <lgrin02@naver.com>
2023-01-20 00:37:35 +0900

    Feat : 코드 에디터 기능 구현 및 디자인 변경(#150)
    업로드,수정 페이지 및 포스트 카드에서 코드에디터가 실행되도록 기능구현하였습니다.
    디테일 페이지에서는 길이를 늘리고 리스트 페이지에서는 최소 높이를 구현하였습니다.
    추후 셀렉트 박스와 코드에디터 버튼을 변경할 예정입니다.

--- src/components/common/card/PostCard.jsx
+++ src/components/common/card/PostCard.jsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import Slider from "react-slick";
-import "./slick/slick.css"
-import "./slick/slick-theme.css"
-import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
-import { python } from '@codemirror/lang-python';
-import { html } from '@codemirror/lang-html';
-import { sql } from '@codemirror/lang-sql';
+import { useParams } from "react-router-dom";
 
+import Slider from "react-slick";
+import "./slick/slick.css";
+import "./slick/slick-theme.css";
+import CodeMirror from "@uiw/react-codemirror";
+import { createTheme } from "@uiw/codemirror-themes";
+import { javascript } from "@codemirror/lang-javascript";
+import { python } from "@codemirror/lang-python";
+import { html } from "@codemirror/lang-html";
+import { sql } from "@codemirror/lang-sql";
+import { darcula, darculaInit } from '@uiw/codemirror-theme-darcula';
+
+import { tags as t } from "@lezer/highlight";
 
 import { customAuthAxios } from "../../../api/customAxios";
 import ProfileCard from "./ProfileCard";
@@ -16,9 +21,9 @@ import style from "./postCard.module.css";
 
 import moreIcon from "../../../assets/imgs/icon-more-vertical.png";
 //import messageIcon from  "../../../assets/imgs/icon-message-small.svg";
-import messageIcon from  "../../../assets/imgs/icon-message.svg";
-import heartIcon from  "../../../assets/imgs/icon-heart.svg";
-import heartFillIcon from  "../../../assets/imgs/icon-fill-heart.svg";
+import messageIcon from "../../../assets/imgs/icon-message.svg";
+import heartIcon from "../../../assets/imgs/icon-heart.svg";
+import heartFillIcon from "../../../assets/imgs/icon-fill-heart.svg";
 //import heartFillIcon from  "../../../assets/imgs/icon-heart-fill.svg";
 import noImg from "../../../assets/imgs/no-picture.png";
 import ModalPortal from "../modal/ModalPortal";
@@ -27,44 +32,54 @@ import MessageModal from "../modal/MessageModal";
 import useCustomModal from "../../../hooks/useCustomModal";
 
 export default function PostCard({ post }) {
+    const { postid } = useParams();
 
     const [isLike, setIsLike] = useState(post.hearted);
     const [likeCount, setLikeCount] = useState(post.heartCount);
     const location = useLocation();
     //토큰 가져오기
     const loginInfo = JSON.parse(localStorage.getItem("loginStorage"));
-    const userId = loginInfo._id;  
-    const accountname = loginInfo.accountname;   
-    const {modalOpen, modalSecondOpen, setModalOpen, setModalSecondOpen, showModal} = useCustomModal();
-    const userType = (post.author._id === userId)? "post" : "your";
-    const [postJson,setPostJson] = useState("");
-    const [postContent, setPostContent]=useState(post.content);
+    const userId = loginInfo._id;
+    const accountname = loginInfo.accountname;
+    const {
+        modalOpen,
+        modalSecondOpen,
+        setModalOpen,
+        setModalSecondOpen,
+        showModal,
+    } = useCustomModal();
+    const userType = post.author._id === userId ? "post" : "your";
+    const [postJson, setPostJson] = useState("");
+    const [postContent, setPostContent] = useState(post.content);
+    const [codeType, setCodeType] = useState();
 
-    useEffect(()=>{
+    useEffect(() => {
         // 코드에디터가 사용 유무 체크
-        if ((post.content.includes("editorOpen"))){
+        if (post.content.includes("editorOpen")) {
             const postJson = JSON.parse(post.content);
-            setPostJson(true)
-            setPostContent(postJson.content)
+            setPostJson(true);
+            setPostContent(postJson.content);
+            handleCodeChange(postJson.codeType);
+            console.log(postJson);
         }
-    },[])
+    }, []);
     const navigate = useNavigate();
-    
+
     const handlePostDetail = () => {
         navigate(`/post/${post.id}`, {
             state: {
-                id : post.id,
-                post: post
-            }
+                id: post.id,
+                post: post,
+            },
         });
-    }
+    };
     const handleProfile = () => {
         navigate(`/profile/${post.author.accountname}`);
-    }
+    };
 
     const handleLikeToggle = () => {
         if (isLike === false) {
-            const setLike = async() => {
+            const setLike = async () => {
                 try {
                     customAuthAxios.post(`/post/${post.id}/heart`);
                     setLikeCount(likeCount + 1);
@@ -74,7 +89,7 @@ export default function PostCard({ post }) {
                     setLikeCount(likeCount);
                     console.error(err);
                 }
-            }
+            };
             setLike();
         } else if (isLike === true) {
             const setUnLike = async () => {
@@ -87,14 +102,14 @@ export default function PostCard({ post }) {
                     setLikeCount(likeCount);
                     console.error(err);
                 }
-            }
+            };
             setUnLike();
         }
-    }
+    };
 
     const handleImgError = (e) => {
         e.target.src = noImg;
-    }
+    };
 
     const settings = {
         dots: true,
@@ -102,104 +117,171 @@ export default function PostCard({ post }) {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false
-    }
+        arrows: false,
+    };
 
     const handlePostModal = (event) => {
         // Input을 체크해서 state를 변경하는 함수.
         if (event.target.name === "수정") {
             navigate(`/postmodify/${post.id}`);
-        }
-        else if (event.target.name === "삭제") {
+        } else if (event.target.name === "삭제") {
             setModalSecondOpen(true);
             setModalOpen(false);
-            }
-        else if (event.target.name === "신고하기"){
+        } else if (event.target.name === "신고하기") {
             handlePostReport();
-            setModalOpen(false)} 
-    }
-    
+            setModalOpen(false);
+        }
+    };
+
     // 포스트 삭제 이벤트
     const handlePostDelete = async () => {
-            try {
-                const postDeleteRes = await customAuthAxios.delete(`/post/${post.id}`);
-                setModalSecondOpen(false);
-                if(location.pathname.split("/")[1]==="post"){
-                    navigate(`/profile/${accountname}`);
-                }
-            } catch (err) {
-                console.error(err);
+        try {
+            const postDeleteRes = await customAuthAxios.delete(
+                `/post/${post.id}`
+            );
+            setModalSecondOpen(false);
+            if (location.pathname.split("/")[1] === "post") {
+                navigate(`/profile/${accountname}`);
             }
-    }
-    
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     // 포스트 신고 이벤트
     const handlePostReport = async () => {
         try {
-            const postReportRes = await customAuthAxios.post(`/post/${post.id}/report`);
+            const postReportRes = await customAuthAxios.post(
+                `/post/${post.id}/report`
+            );
             alert("신고됐습니다. 운영진이 확인 후 삭제하겠습니다.");
             setModalSecondOpen(false);
         } catch (err) {
             console.error(err);
         }
-    }
+    };
+
+    const handleCodeChange = (type) => {
+        switch (type) {
+            case "html":
+                setCodeType([html({ jsx: true })]);
+                break;
+            case "javascript":
+                setCodeType([javascript({ jsx: true })]);
+                break;
+            case "sql":
+                setCodeType([sql({ jsx: true })]);
+                break;
+            case "python":
+                setCodeType([python({ jsx: true })]);
+                break;
+            case "default":
+                setCodeType();
+                break;
+            default:
+                break;
+        }
+    };
+
 
     return (
         <article className={style.article_post_card}>
             <div className={style.cont_post_author}>
-                <ProfileCard profileImg={post.author.image} profileState="post" profileName={post.author.username} profileCont={post.author.accountname} handleBtn={handleProfile}/>
+                <ProfileCard
+                    profileImg={post.author.image}
+                    profileState="post"
+                    profileName={post.author.username}
+                    profileCont={post.author.accountname}
+                    handleBtn={handleProfile}
+                />
             </div>
-            {
-                post.image ? (
-                    <Slider {...settings}>
-                    {
-                        post.image.split(",").map((imgsrc, index) => 
-                            <img key={index} src={imgsrc} alt="" onClick={handlePostDetail} onError={handleImgError}/>
-                        )
-                        
-                    }
-                    </Slider>
-                ) : null
-            }
+            {post.image ? (
+                <Slider {...settings}>
+                    {post.image.split(",").map((imgsrc, index) => (
+                        <img
+                            key={index}
+                            src={imgsrc}
+                            alt=""
+                            onClick={handlePostDetail}
+                            onError={handleImgError}
+                        />
+                    ))}
+                </Slider>
+            ) : null}
             <div className={style.cont_post_content}>
-                <p className={style.post_contents}  onClick={handlePostDetail}>{postContent}</p>
+                <p className={style.post_contents} onClick={handlePostDetail}>
+                    {postContent}
+                </p>
+                {postJson && (
+                    <div>
+                        <CodeMirror
+                            value={JSON.parse(post.content).code}
+                            maxHeight={postid ? "2000px" : "200px"}
+                            width="340px"
+                            readOnly="true"
+                            extensions={codeType}
+                            theme={darculaInit({
+                                settings: {
+                                    lineHighlight: "transparent",
+                                    caret: "#c6c6c6",
+                                    fontFamily: "monospace",
+                                    gutterBackground:"#181226"
+                                },
+                            })}
+                        />
+                    </div>
+                )}
                 <div className={style.box_btn}>
-                    <img src={isLike ? heartFillIcon : heartIcon} alt="좋아요" className={style.btn_like} onClick={handleLikeToggle}/>
-                    <span className={style.span_count} >{likeCount}</span>
-                    <img src={messageIcon} alt="댓글" onClick={handlePostDetail}/>
-                    <span className={style.span_count} onClick={handlePostDetail}>{post.comments.length}</span>
+                    <img
+                        src={isLike ? heartFillIcon : heartIcon}
+                        alt="좋아요"
+                        className={style.btn_like}
+                        onClick={handleLikeToggle}
+                    />
+                    <span className={style.span_count}>{likeCount}</span>
+                    <img
+                        src={messageIcon}
+                        alt="댓글"
+                        onClick={handlePostDetail}
+                    />
+                    <span
+                        className={style.span_count}
+                        onClick={handlePostDetail}
+                    >
+                        {post.comments.length}
+                    </span>
                 </div>
                 <span className={style.span_post_date}>
-                    {
-                        new Intl.DateTimeFormat("ko", { dateStyle: "long" }).format(new Date(post.createdAt))
-                    }
+                    {new Intl.DateTimeFormat("ko", {
+                        dateStyle: "long",
+                    }).format(new Date(post.createdAt))}
                 </span>
-                {postJson &&  
-                        <div>
-                            <CodeMirror
-                                value={JSON.parse(post.content).code}
-                                height="200px"
-                                width="340px"
-                                readOnly= "true"
-                                // extensions={JSON.parse(post.content).codeType}
-                            />
-                        </div>}
             </div>
-            <img src={moreIcon} className={style.btn_more} alt="더보기" onClick={showModal}/>
+            <img
+                src={moreIcon}
+                className={style.btn_more}
+                alt="더보기"
+                onClick={showModal}
+            />
             <ModalPortal>
-                {modalOpen && 
-                    <Modal  type={userType} 
-                            modalOpen={modalOpen}
-                            setModalOpen={setModalOpen} 
-                            handleModal ={handlePostModal} />}    
-                {modalSecondOpen &&
-                    <MessageModal  
-                            type="post" 
-                            modalOpen={modalSecondOpen}
-                            setModalOpen={setModalSecondOpen} 
-                            handleHeaderBtn={showModal}
-                            handleModal ={handlePostDelete} />} 
-            </ModalPortal>    
+                {modalOpen && (
+                    <Modal
+                        type={userType}
+                        modalOpen={modalOpen}
+                        setModalOpen={setModalOpen}
+                        handleModal={handlePostModal}
+                    />
+                )}
+                {modalSecondOpen && (
+                    <MessageModal
+                        type="post"
+                        modalOpen={modalSecondOpen}
+                        setModalOpen={setModalSecondOpen}
+                        handleHeaderBtn={showModal}
+                        handleModal={handlePostDelete}
+                    />
+                )}
+            </ModalPortal>
         </article>
-    )
+    );
 }
-
--- src/pages/uploadPage/PostModify.jsx
+++ src/pages/uploadPage/PostModify.jsx
@@ -1,10 +1,17 @@
-
-import { React, useState, useRef, useEffect } from "react";
+import { React, useState, useRef, useEffect, useCallback } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import imageCompression from 'browser-image-compression';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { python } from '@codemirror/lang-python';
+import { html } from '@codemirror/lang-html';
+import { sql } from '@codemirror/lang-sql';
+import { java } from '@codemirror/lang-java';
+import { darcula, darculaInit } from '@uiw/codemirror-theme-darcula';
 
 import style from "./upload.module.css";
 import UploadImg from "../../assets/imgs/upload-img.png";
+import CodeImg from "../../assets/imgs/coding.png";
 import UploadPhoto from "../../components/uploadPhoto/UploadPhoto";
 import Header from "../../components/common/header/Header";
 import ProfileCard from "../../components/common/card/ProfileCard";
@@ -24,9 +31,11 @@ export default function PostModify() {
     const myAccountname = loginInfo.accountname
     const [profileImg, setProfileImg] = useState("");
     const [isActive, setIsActive] = useState(false);
+    const [editorOpen, setEditorOpen] = useState(false);
     const [postText, setPostText] = useState("");
     const [IsValue, setIsValue] = useState(false); // 저장 버튼 활성화를 위해 게시글 작성 유무
     const [resizeImage, setResizeImage] = useState();
+    const select = useRef();
 
     const [fileName, setFileName] = useState([]); //api에서 인코딩한 파일이름
     const [previewImgUrl, setPreviewImgUrl] = useState([]); //미리보기 이미지 src
@@ -35,17 +44,24 @@ export default function PostModify() {
     const {postid} = useParams();
     const textRef = useRef();
     const fileRef = useRef();
-    const [post, setPost] = useState();
     const [isError, setIsError] = useState(false);
     const [errorMsg, setErrorMsg] = useState("");
+    const [postJson,setPostJson] = useState("");
+    const [codeType, setCodeType] = useState();
+    const [codeSelected, setCodeSelected] = useState();
+    const [content, setContent] = useState("");
+    const [ res , getRes ] = useState("");
 
     //기존 포스트 데이터 요청
     useEffect(() => {
     async function getPost() {
         try {
             const res = await customAuthAxios.get(`/post/${postid}`);
+
             setPostText(res.data.post.content);
-            setIsLoading(false)
+            console.log(res.data.post.content);
+            // 코드에디터가 사용 유무 체크
+            setIsLoading(false);
             setFileName(res.data.post.image.split(","));
 
             if (res.data.post.image !== "") 
@@ -68,16 +84,26 @@ export default function PostModify() {
         if(postText)  {
             setIsValue(true)
             handleResizeHeight();
+            if ((postText.includes("editorOpen"))){
+                const postJson = JSON.parse(postText);
+                setEditorOpen(true);
+                setPostJson(true);
+                setPostText(postJson.content);
+                getRes(postJson.code);
+                setCodeSelected(postJson.codeType);
+                handleHightingCode(postJson.codeType)
+            }
         }
         else setIsValue(false);
     },[postText])
 
-    function handleResizeHeight() {
+    const handleResizeHeight = (e) => {
+
         textRef.current.style.height = "auto";
         textRef.current.style.height = textRef.current.scrollHeight + "px";
     }
     
-    function handleText(e) {
+    const handleText = (e) => {
         setPostText(e.target.value);
         if (e.target.value) {
             setIsActive(true);
@@ -175,13 +201,34 @@ export default function PostModify() {
         fileRef.current.value = "";
     }
 
+        const getContent = () => {
+        if(editorOpen){
+            setContent(
+                JSON.stringify({   
+                    content : postText,
+                    editorOpen : editorOpen,
+                    codeType : codeSelected,
+                    code : res
+                })
+            )
+        }else{
+            setContent(
+                postText
+            )
+        }
+    }
+
+    useEffect(()=>{
+        getContent();
+    },[postText, editorOpen, codeSelected, res])
+
     //게시글 업로드 버튼 클릭 시 PUT
     function handleSubmit(e) {
         e.preventDefault();
         if(IsValue){
             const postData = {
                 post: {
-                content: textRef.current.value,
+                content: content,
                 image: fileName.join(","),
                 },
             };
@@ -197,6 +244,65 @@ export default function PostModify() {
             navigate(`/profile/${myAccountname}`);
             }
         }
+
+        //코드에디터
+
+    const onChange = useCallback((value, viewUpdate) => {
+        getRes(value)
+    }, []); 
+
+    const handleEditor = () =>{
+        editorOpen ? setEditorOpen(false) : setEditorOpen(true)
+        setCodeType()
+    }
+
+    const handleCodeChange = (e)=>{
+        
+        setCodeSelected(e.target.value)
+    }
+    
+    useEffect(() => {
+        handleHightingCode();
+    }, [codeSelected])
+
+    const handleHightingCode = () => {
+        
+        switch(codeSelected) {
+            case 'html':
+            setCodeType(
+                [html({jsx: true })]
+                );
+                break;
+            case 'javascript': 
+            setCodeType(
+                [javascript({jsx: true })]
+                );
+                break;
+            case 'sql': 
+            setCodeType(
+                [sql({jsx: true })]
+                );
+                break;
+            case 'python': 
+                setCodeType(
+                    [python({jsx: true })]
+                    );
+                    break;  
+            case 'java': 
+                setCodeType(
+                [java({jsx: true })]
+                );
+                        break;  
+                        
+            case 'default': 
+            setCodeType(
+                
+                );
+                break;     
+            default:
+                break;
+        }
+    }
 
     if(isLoading) {
         return <Loading />
@@ -221,7 +327,33 @@ export default function PostModify() {
                         required
                     />
                 </div>
-                {/* <img src={imgUrl} alt="" /> */}
+                {editorOpen &&  
+                        <div>
+                            <select name="selectCode" value={codeSelected} className={style.code_select} onChange={handleCodeChange}>
+                                <option value = "default">text</option>
+                                <option value = "html">html</option>
+                                <option value = "java">java</option>
+                                <option value = "sql">sql</option>
+                                <option value = "javascript">javascript</option>
+                                <option value = "python">python</option>
+                            </select>
+                            <CodeMirror
+                                value={res}
+                                height="200px"
+                                width='390px'
+                                extensions={codeType}
+                                onChange={onChange}
+                                theme={darculaInit({
+                                    settings: {
+                                        lineHighlight: "transparent",
+                                        caret: "#c6c6c6",
+                                        fontFamily: "monospace",
+                                        gutterBackground:"#181226"
+                                    },
+                                })
+                            }
+                            />
+                        </div>}
                 <UploadPhoto type = "modi" imageFileList={previewImgUrl} handleRemoveImg={deletePreview}/>
                 {view === "pending" ?<marquee   className={style.p_uploadImg}
                                                 scrollamount="10">이미지 업로드 중</marquee>  : ""}
@@ -243,6 +375,12 @@ export default function PostModify() {
                         alt="업로드 버튼"
                     />
                 </label> 
+                <img
+                    onClick={handleEditor}
+                    className={style.btn_code}
+                    src={CodeImg}
+                    alt="업로드 버튼"
+                    />
             </div>
             
         </>)
